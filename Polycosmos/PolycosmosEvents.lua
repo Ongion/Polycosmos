932adddbf8c43b3480c5822e6e5f58afd5b51130
Benjamin Paul [MSFT] <benjamin.ian.paul@gmail.com>
2024-05-10 10:37:57 -0400

    Rename variable, move processing inside SendDeathlink

--- Polycosmos/PolycosmosEvents.lua
+++ Polycosmos/PolycosmosEvents.lua
@@ -41,7 +41,7 @@ last_room_completed=0
 --- variables for deathlink checks
 
 is_greece_death = false
-should_send_deathlink = true
+is_deathlink_caused_of_death = false
 
 --variable for avoid racing problems
 
@@ -310,9 +310,9 @@ function PolycosmosEvents.KillPlayer( message )
         CurrentRun.Hero.Health = 0
         CheckLastStand(CurrentRun.Hero, { })
     else
-        should_send_deathlink = false
+        is_deathlink_cause_of_death = true
         KillHero(CurrentRun.Hero, { }, { })
-        should_send_deathlink = true
+        is_deathlink_cause_of_death = false
     end
 end
 
@@ -323,16 +323,14 @@ StyxScribe.AddHook( PolycosmosEvents.KillPlayer, styx_scribe_recieve_prefix.."De
 function PolycosmosEvents.SendDeathlink()
     if (is_greece_death == true) then
         is_greece_death = false
-    else
+    else if (is_deathlink_cause_of_death == false)
         StyxScribe.Send(styx_scribe_send_prefix.."Zag died")
     end
 end
 
 
 ModUtil.Path.Wrap("HandleDeath", function( baseFunc, currentRun, killer, killingUnitWeapon )
-    if should_send_deathlink then
-	    PolycosmosEvents.SendDeathlink()
-    end
+	PolycosmosEvents.SendDeathlink()
 	return baseFunc(currentRun, killer, killingUnitWeapon)
 end)
 
